0a79aa7999b3a1179302efbd661208c59c67364e
Rogliano Antoine <inateno@gmail.com>
2018-10-01 11:26:03 +0200

    aller plus loin 1 - fichier commun

---
+++ global.rollup.config.js
@@ -0,0 +1,46 @@
+const GLOBAL_CONFIG = {
+  babel: {
+    exclude: "node_modules/**"
+  }
+};
+
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import autoExternal from 'rollup-plugin-auto-external';
+
+import pkg from './package.json';
+
+export default function generateConfig( optional ) {
+  
+  
+  return {
+    input: optional.input || 'src/index.js',
+    output: [
+      {
+        file: optional.main || "dist/index.js",
+        format: 'cjs'
+      },
+      {
+        file: optional.module || "dist/index.es.js",
+        format: 'es'
+      }
+    ],
+    external: optional.external || [],
+    plugins: [
+      // say to rollup how to resolve node dependencies
+      resolve(),
+      // allow to include all kind of amd package in an ES6 way
+      commonjs( Object.assign( optional.commonjs || {}, {
+        include: 'node_modules/**',
+        namedExports: {
+          'node_modules/react/index.js': ['Component', 'PureComponent', 'Fragment', 'Children', 'createElement', 'cloneElement']
+        }
+      } ) ),
+      // babel
+      babel( Object.assign( optional.babel || {}, GLOBAL_CONFIG.babel ) ),
+      // automatically detect what should be excluded from the build
+      autoExternal()
+    ]
+  };
+}
--- packages/alpha/rollup.config.js
+++ packages/alpha/rollup.config.js
@@ -1,37 +1,6 @@
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import autoExternal from 'rollup-plugin-auto-external';
+import pkg from './package.json'
+import generateConfig from "../../global.rollup.config.js";
 
-import pkg from './package.json';
-
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs'
-    },
-    {
-      file: pkg.module,
-      format: 'es'
-    }
-  ],
-  external: [
-  ],
-  plugins: [
-    // say to rollup how to resolve node dependencies
-    resolve(),
-    // allow to include all kind of amd package in an ES6 way
-    commonjs({
-      include: 'node_modules/**',
-      namedExports: {
-        'node_modules/react/index.js': ['Component', 'PureComponent', 'Fragment', 'Children', 'createElement', 'cloneElement']
-      }
-    }),
-    // babel
-    babel({exclude: "node_modules/**"}),
-    // automatically detect what should be excluded from the build
-    autoExternal()
-  ]
-}
+export default generateConfig( {
+  external: []
+} );
